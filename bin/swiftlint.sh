85845e5d14958aa28516a0edcdee875e32f0145a
Jeroen Wesbeek <github@osx.eu>
2018-07-24 23:56:54 +0200

    Updated swiftlint

--- bin/swiftlint.sh
+++ bin/swiftlint.sh
@@ -8,7 +8,7 @@ swiftlint() {
       set -e
 
       local SWIFTLINT_PKG_PATH="/tmp/SwiftLint.pkg"
-      local SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.24.2/SwiftLint.pkg"
+      local SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.26.0/SwiftLint.pkg"
 
       wget --output-document=$SWIFTLINT_PKG_PATH $SWIFTLINT_PKG_URL
 
